c1fcd168029ba4849b2df21c4e337f30ccf53a33
HearLing <1402690276@qq.com>
2022-05-08 10:17:08 +0800

    github搜索案例

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,30 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Search /> 
+    <PersonsList/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Search from './components/Search.vue'
+import PersonsList from './components/PersonsList'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Search,
+    PersonsList
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  width: 70%;
   color: #2c3e50;
-  margin-top: 60px;
+  border: 1px solid gray;
+  text-align: center;
+  margin: 50px auto;
+  padding: 20px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -5,4 +5,8 @@ Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+   // 全局安装事件总线
+   beforeCreate(){
+    Vue.prototype.$bus = this
+  }
 }).$mount('#app')
